4cc832cd8d9af489525da89bb577c02764a8ac5d
McKayla Lankau <lankaukk@yahoo.com>
2021-08-10 00:32:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let element = document.querySelector('main#main');
+
+element.remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = 'McKayla is the champion!'
